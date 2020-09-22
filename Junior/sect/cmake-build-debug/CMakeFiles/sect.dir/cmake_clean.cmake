ae7b201e32f6bdcb7805b31801c58d29549d7462
Serellka <44942855+Serellka@users.noreply.github.com>
2020-09-22 23:14:49 +0300

    Add files via upload

---
+++ Junior/sect/cmake-build-debug/CMakeFiles/sect.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sect.dir/main.cpp.obj"
+  "libsect.dll.a"
+  "sect.exe"
+  "sect.exe.manifest"
+  "sect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
